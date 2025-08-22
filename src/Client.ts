a8380c2090083fee9176934a621e9ae8e7eae577
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-22 19:19:32 +0000

    Release 0.2.0

--- src/Client.ts
+++ src/Client.ts
@@ -70,8 +70,8 @@ export class BridgeApiClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "@usebridge/api",
-                    "X-Fern-SDK-Version": "0.1.5",
-                    "User-Agent": "@usebridge/api/0.1.5",
+                    "X-Fern-SDK-Version": "0.2.0",
+                    "User-Agent": "@usebridge/api/0.2.0",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
