5e106f02e4003c5081b779f6fc6f64bebee41010
Alan Huynh <tamalhu@gmail.com>
2025-11-18 12:43:42 -0800

    Map the environment name to its URL value
    The Fern API client uses URLs for "environments"

---
+++ src/WrappedClient.ts
@@ -0,0 +1,30 @@
+import { BridgeApiEnvironment } from 'environments.js';
+import { BridgeApiClient as FernClient } from "./Client.js";
+
+export class WrappedClient extends FernClient {
+    constructor(options: FernClient.Options) {
+        super({
+            ...options,
+            environment: normalizeEnvironment(options.environment),
+        });
+    }
+}
+
+function normalizeEnvironment(environment?: FernClient.Options["environment"]): FernClient.Options["environment"] {
+    if (!environment || typeof environment === "function") return environment
+
+    // We can assume that environment is a plain string here; we check above for
+    // functions, so this should be safe
+    const env = environment as unknown as string;
+    
+    // Convert any environment names to their BridgeApiEnvironment values
+    // Otherwise, just use the environment as-is
+    switch (env.toLowerCase()) {
+        case "production":
+            return BridgeApiEnvironment.Production;
+        case "sandbox":
+            return BridgeApiEnvironment.Sandbox;
+        default:
+            return env;
+    }
+}
